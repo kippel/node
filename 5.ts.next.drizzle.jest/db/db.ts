19f35b00d39ffc24c9735701026c9798931c3617
kippel <kippeld@gmail.com>
2025-05-30 15:18:18 +0200

    next test

--- 5.ts.next.drizzle.jest/db/db.ts
+++ 5.ts.next.drizzle.jest/db/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import * as schema from "../db/schema";
+import * as schema from "./schema";
 
-const sqlite = new Database("db/sqlite.db");
+const sqlite = new Database("./db/sqlite.db");
 export const db = drizzle(sqlite, { schema });
 export { schema };
--- 5.ts.next.drizzle.jest/src/app/page.tsx
+++ 5.ts.next.drizzle.jest/src/app/page.tsx
@@ -1,13 +1,12 @@
- 
-import { db } from '../db/db'
-import { users } from '../db/schema'
+import { db } from '../../db/db'
+import { users } from '../../db/schema'
 
 export default function Home() {
 
   const user = db.select().from(users).all()
   console.log(user)
   return (
-    <div>foo 
+    <div> foo
     </div>
   );
 }
