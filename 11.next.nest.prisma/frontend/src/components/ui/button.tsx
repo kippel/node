c9189beb82b0c86298508ec56af8809427c207c1
kippel <kippel>
2025-07-07 07:49:34 +0200

    doc

--- 11.next.nest.prisma/frontend/src/components/ui/button.tsx
+++ 11.next.nest.prisma/frontend/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-       locked:
+        locked:
           "bg-neutral-200 text-primary-foreground hover:bg-neutral-200/90 border-neutral-400 border-b-4 active:border-b-0",
         default:
           "bg-primary-500 text-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500 rounded-lg",
