83876550ea4ee67dadd15b3c5af2173c794a9246
kippel <kippeld@gmail.com>
2025-07-25 09:04:27 +0200

    next fastapi

---
+++ 12.next.fastapi/backend/api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import auth
+
+from .database import Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+from api.models import User
+from api.deps import db_dependency
+@app.get("/")
+def root(db: db_dependency):
+
+    user = db.query(User).all()
+
+    return {"user": user}
+
+app.include_router(auth.router)
