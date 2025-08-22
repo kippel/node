06fb470d07ebaa8cfe045322550ab915f4e43269
kippel <kippeld@gmail.com>
2025-08-22 07:19:10 +0200

    16

---
+++ 16.next.next-intl/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import {useTranslations} from 'next-intl';
+
+export default function Home() {
+  const t = useTranslations('HomePage');
+  return <h1>{t('title')}</h1>;
+}
